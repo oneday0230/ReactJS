2be6b0653f3b8224679e096155483bd9ccff2c9b
hyo jin nam <blackswan1989@naver.com>
2023-01-02 23:55:37 +0900

    state & form
    with typescript

--- react-master/src/App.tsx
+++ react-master/src/App.tsx
@@ -1,11 +1,26 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() 
 {
+  const [value, setValue] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: {value},
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value); // submit 버튼 클릭시 콘솔에 보여준다.
+  }
+
   return (
     <div>
-      <Circle borderColor="black" bgColor="teal"></Circle>
-      <Circle text="I'm here" bgColor="tomato"></Circle>
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder="username"></input>
+        <button>Log in</button>
+      </form>
     </div>
   )
 }
