b5a98d97e1f7070f80c111480e3efde49c4c5178
HyoJin <121423610+oneday0230@users.noreply.github.com>
2023-01-03 00:35:42 +0900

    add comment

--- react-master/src/App.tsx
+++ react-master/src/App.tsx
@@ -5,14 +5,14 @@ function App()
   const [value, setValue] = useState('');
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
-      currentTarget: {value},
+      currentTarget: {value},   //// const value = event.currentTarget.value 와 같은 코드 (여러 event를 가져올 때 유용하다 아래 주석 참조)
     } = event;
     setValue(value);
   };
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("Hello", value); // submit 버튼 클릭시 콘솔에 보여준다.
+    console.log("Hello", value); //// submit 버튼 클릭시 콘솔에 보여준다.
   }
 
   return (
@@ -27,4 +27,16 @@ function App()
 
 export default App;
 
-// git test
+
+/*
+const value = event.currentTarget.value;
+const tagName = event.currentTarget.tagName;
+const width = event.currentTarget.width;
+const id = event.currentTarget.id;
+
+위 코드를 아래처럼 작성 가능/
+
+const {
+currentTarget: {value, tagName, width, id}
+} = event;
+*/
