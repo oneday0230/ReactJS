15c841372b16affe692e603fc1a2c77c3c12c404
HyoJin <121423610+oneday0230@users.noreply.github.com>
2023-01-03 00:03:47 +0900

    State & Froms with TypeScript
    #3.4~3.5

--- react-master/src/App.tsx
+++ react-master/src/App.tsx
@@ -2,9 +2,25 @@ import { useState } from "react";
 
 function App() 
 {
+  const [value, setValue] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: {value},
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value); // submit 버튼 클릭시 콘솔에 보여준다.
+  }
+
   return (
     <div>
-
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder="username"></input>
+        <button>Log in</button>
+      </form>
     </div>
   )
 }
