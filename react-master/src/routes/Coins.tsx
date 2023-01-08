dfafb2a8799df295f450b6be3ea70e5ac9612aa7
HyoJin <121423610+oneday0230@users.noreply.github.com>
2023-01-08 23:50:30 +0900

    CRYPTO TRACKER - Home part2 set API
    # 5.3 set useEffect, async & await, loading view

--- react-master/src/routes/Coins.tsx
+++ react-master/src/routes/Coins.tsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useEffect, useState } from "react";
 
 const Container = styled.div`
     padding: 0 20px;
+    max-width: 480px;
+    margin: 0 auto;
 `;
 const Header = styled.header`
-    height: 10vh;
+    height: 15vh;
     display: flex;
     justify-content: center;
     align-items: center;
+    margin: 20px 0;
+`;
+const Title = styled.h1`
+    font-size: 48px;
+    margin: 40px 0;
+    color: ${props => props.theme.accentColor};
+`;
+const Loader = styled.div`
+    text-align: center;
+    margin-top: 60px;
 `;
 const CoinsList = styled.ul`
 `;
@@ -28,55 +41,48 @@ const Coin = styled.li`
     }
    }
 `;
-const Title = styled.h1`
-    font-size: 48px;
-    color: ${props => props.theme.accentColor}
-`;
 
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-        },
-        {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-        },
-        {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-    },
-];
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+};
 
 function Coins()
 {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    // UseEffect를 사용하면 특정 시점에 코드를 실행시킬 수 있다.
+    useEffect(() => {
+        (async() => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");   //  coin 정보를 가져오는 api
+            const json = await response.json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+        })();   
+        // ()를 바로 붙이면 함수를 바로 실행시킬 수 있다.
+    }, [])
     return (
         <Container>
             <Header>
                 <Title>Coin</Title>
             </Header>
-            <CoinsList>
-                {coins.map(coin => (
-                    <Coin key={coin.id}>
-                        <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
-                    </Coin>
-                ))}
-            </CoinsList>
+            {
+                loading ? (<Loader>Loading...</Loader>) : (
+                    <CoinsList>
+                        {coins.map(coin => (
+                            <Coin key={coin.id}>
+                                <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                            </Coin>
+                        ))}
+                    </CoinsList>
+                )
+            }
         </Container>
     );
 }
